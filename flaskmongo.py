b59a2bc41c5841804561a35501a16aa1576fd330
root <root@ip-172-31-11-156.ap-south-1.compute.internal>
2021-05-13 10:51:45 +0000

    fifth commit

--- flaskmongo.py
+++ flaskmongo.py
@@ -7,6 +7,9 @@ app = Flask("my-app")
 client = MongoClient("mongodb://127.0.0.1:27017")
 collection=client.userdb.u_collection
 
+get_data = lambda x : request.args.get(x)
+  
+
 @app.route("/home")
 def welcome():
   #this is the homepage
@@ -27,16 +30,21 @@ def delete():
   #this is for deleting stuff
   return render_template("delete.html")
 
+@app.route("/fetchdata")
+def fetch_data():
+  #this method is used to fetch data
+  return render_template("fetch.html")
+
 @app.route("/addop" , methods=["GET"])
 def add_to_db():
   #this function adds the data to database
 
   doc={ 
-        "_id": request.args.get("id"),
-        "name": request.args.get("na"),
-        "age": int(request.args.get("ag")),
-        "region": request.args.get("reg"),
-        "weight": request.args.get("we")
+        "_id": get_data("id"),
+        "name": get_data("na"),
+        "age": int(get_data("ag")),
+        "region": get_data("reg"),
+        "weight": get_data("we")
   }
 
   collection.insert_one(doc)
@@ -47,8 +55,8 @@ def update_data():
   #this function updates record already present
 
   collection.update_one(
-             {"_id":request.args.get("idd")},
-             {"$set":{request.args.get("sel"):request.args.get("nv")}}
+             {"_id":get_data("idd")},
+             {"$set":{get_data("sel"):get_data("nv")}}
   )
 
   return "Updated!"
@@ -57,8 +65,26 @@ def update_data():
 def delete_data():
   #this function deletes the document
 
-  collection.delete_one({ "_id":request.args.get("id1") })
+  collection.delete_one({ "_id":get_data("id1") })
   return "Deleted!"
+
+@app.route("/fad")
+def get_all_data():
+  #this prints the whole table
+  
+  out = ''
+  for record in collection.find():
+    out += str(record) + "</br>"
+
+  return out
+
+@app.route("/fsd" , methods=["GET"])
+def get_doc():
+  #this will print a document data based on ID
+   
+  query =  { "_id" : get_data("sd") }
+  doc = str(collection.find_one(query))
+  return doc
 
   
 app.run(host = "0.0.0.0", port = 3882)
