41eb13841f0ae3531f527fe0d6ce8f1d3ac81747
root <root@ip-172-31-11-156.ap-south-1.compute.internal>
2021-05-13 05:57:33 +0000

    second commit

--- flaskmongo.py
+++ flaskmongo.py
@@ -3,6 +3,10 @@ from pymongo import MongoClient
 
 app = Flask("my-app")
 
+#connecting with database
+client = MongoClient("mongodb://127.0.0.1:27017")
+collection=client.userdb.u_collection
+
 @app.route("/home")
 def welcome():
   #this is the homepage
@@ -22,6 +26,20 @@ def update_record():
 def delete():
   #this is for deleting stuff
   return render_template("delete.html")
-  
 
+@app.route("/addop" , methods=["GET"])
+def add_to_db():
+  #this function adds the data to database
+
+  doc={
+        "name": request.args.get("na"),
+        "age": int(request.args.get("ag")),
+        "region": request.args.get("reg"),
+        "weight": request.args.get("we")
+  }
+
+  collection.insert_one(doc)
+  return "Data Added!"
+
+  
 app.run(host = "0.0.0.0", port = 3882)
